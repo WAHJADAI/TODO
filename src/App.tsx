35b5477b566905b21fd9a02000eeff8cb8ff5168
unknown <drafzaza123@icloud.com>
2023-08-23 21:00:06 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p>TODO</p>
+      <form>
+        <input type="checkbox" name="List1" id="List1" />
+      </form>
+    </>
+  )
+}
+
+export default App
