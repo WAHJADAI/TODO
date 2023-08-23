31716d88bd0c533800ab21c6a7b61dab1fe6faf2
unknown <drafzaza123@icloud.com>
2023-08-23 21:46:44 +0700

    add button create todo ,input text new todo and button delete

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,12 @@ function App() {
   return (
     <>
       <p>TODO</p>
+      <label htmlFor="NewList">Add New List</label>
+      <input type="text" name="NewText" id="NewText" />
+      <input type="submit" value="submit" />
       <form>
         <input type="checkbox" name="List1" id="List1" />
+        <input type="button" value="Delete" />
       </form>
     </>
   )
