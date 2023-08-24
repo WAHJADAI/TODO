ca6e885f1a2426be4ae0279541afc1004c12eda4
unknown <drafzaza123@icloud.com>
2023-08-24 20:01:13 +0700

    click add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { item } from './todo'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  
-
+  const [todos,setTodos]=useState<item[]>([]);
+  const [task,setTask]=useState<string>("")
+  function handleAddTask (){
+    const newTask:item ={id:Date.now(),text:task,complete:false}
+    setTodos([...todos,newTask])
+  }
   return (
-    <>
-      <p>TODO</p>
-      <label htmlFor="NewList">Add New List</label>
-      <input type="text" name="NewText" id="NewText" />
-      <input type="submit" value="submit" />
-      <form>
-        <input type="checkbox" name="List1" id="List1" />
-        <input type="button" value="Delete" />
-      </form>
-    </>
+    <div>
+      <div>
+        <h1>TODO LIST</h1>
+      <div>Add Task</div>
+      <input type="text" name="task"  onChange={(e)=>setTask(e.currentTarget.value)}/>
+      <button onClick={handleAddTask}>Add</button>
+      <ul>
+        {todos.map((todo)=>(
+          
+          <li key={todo.id}><input type="checkbox" key={todo.id} />{todo.text}<button>delete</button></li>
+        ))}
+      </ul>
+      </div>
+    
+
+      
+    </div>
   )
 }
 
