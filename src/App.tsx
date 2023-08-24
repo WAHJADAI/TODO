c2f184fe3d3067a8d9bf88df3c7cb1dfd60e2564
unknown <drafzaza123@icloud.com>
2023-08-24 20:11:31 +0700

    alert when add task emty

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ function App() {
   const [todos,setTodos]=useState<item[]>([]);
   const [task,setTask]=useState<string>("")
   function handleAddTask (){
-    const newTask:item ={id:Date.now(),text:task,complete:false}
-    setTodos([...todos,newTask])
+    if(task !==""){const newTask:item ={id:Date.now(),text:task,complete:false}
+    setTodos([...todos,newTask])}
+    else alert("please write the Message.")
   }
   return (
     <div>
